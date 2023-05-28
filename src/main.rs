fc4b72e801052360cfd1084108f1829bc27613b0
Jesus Jayaro <jayaro@gmail.com>
2023-05-28 16:58:28 -0600

    refactor, working license parser

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,136 @@
 use std::env;
 use std::fs::File;
-use std::io::{BufReader, BufRead};
-use downloader::Downloader;
+use std::io::{BufReader, BufRead, Write};
+use reqwest;
+use serde::{Serialize, Deserialize};
 
-fn main() {
-    //Arguments
-    let args: Vec<String> = env::args().collect();
-
-    let filename = &args[1];
-
-    println!("In file {}", filename);
-
-    //Downloader
-    let url = format!("https://static.aer.ca/prd/data/well-lic/{}.TXT", filename);
-
-    let mut downloader = Downloader::builder()
-        .download_folder(std::path::Path::new("TXT"))
-        .parallel_requests(1)
-        .build()
-        .unwrap();
-
-    let dl = downloader::Download::new(&url);
-
-    let result = downloader.download(&[dl]).unwrap();
-
-    for r in result {
-        match r {
-            Err(e) => print!("Error occurred! {}", e.to_string()),
-            Ok(s) => print!("Success: {}", &s),
-        };
-    }
-
-    let lines = open_file_lines(filename);
-
-    let index = Indeces::search(&lines);
-
-    let date = &lines[index.date[0]].trim()[6..];
-
-    if lines.len() > 20 {
-        let licences = licences(&lines, &index.breaks);
-        writer(licences, &date, 17);
-    };
-    
-    if index.cancelled.len() > 0 {
-        let cancelled = cancelled(&lines, &index.breaks, &index.cancelled);
-        writer(cancelled, &date, 2);
-    }
-
+#[derive(Debug, Serialize, Deserialize)]
+struct License {
+    date: String,
+    well_name: String,
+    licence_number: String,
+    mineral_rights: String,
+    ground_elevation: String,
+    unique_identifier: String,
+    surface_coordinates: String,
+    aer_field_centre: String,
+    projected_depth: String,
+    aer_classification: String,
+    field: String,
+    terminating_zone: String,
+    drilling_operation: String,
+    well_purpose: String,
+    well_type: String,
+    substance: String,
+    licensee: String,
+    surface_location: String,
 }
 
+async fn download_file(filename: String) -> Result<(), Box<dyn std::error::Error>> {
+    let url = format!("https://static.aer.ca/prd/data/well-lic/WELLS{}.TXT", filename);
+
+    let response = reqwest::get(&url).await?;
+
+    let bytes = response.bytes().await?;
+
+    let mut file = File::create(format!("TXT/WELLS{}.TXT", filename))?;
+
+    file.write_all(&bytes)?;
+
+    Ok(())
+}
+
+fn open_file_lines(filename: &str) -> Vec<String> {
+    let path = format!("TXT/WELLS{}.TXT", filename);
+    let file = File::open(path).expect("File not found");
+    let content = BufReader::new(file);
+    let lines: Vec<String> = content
+        .lines()
+        .map(|line| line.expect("Something went wrong"))
+        .collect();
+
+    lines
+}
+
+fn trim_and_remove_empty_lines(lines: Vec<String>) -> Vec<String> {
+    let mut lines_trimmed: Vec<String> = Vec::new();
+
+    for line in lines {
+        if line.trim().len() > 0 {
+            lines_trimmed.push(line.trim().to_string());
+        }
+    }
+
+    lines_trimmed
+}
+
+fn extract_licences_lines(lines: &Vec<String>, breaks: Vec<usize>) -> Vec<String> {
+    let mut licences_lines: Vec<String> = Vec::new();
+
+    for i in breaks[1]+1..breaks[2]-1 {
+        licences_lines.push(lines[i].to_string());
+    }
+
+    licences_lines
+}
+fn find_every_5th_row(lines: &Vec<String>) -> Vec<usize> {
+    let mut every_5th_row: Vec<usize> = Vec::new();
+
+    for (pos, e) in lines.iter().enumerate() {
+        if pos % 5 == 0 {
+            every_5th_row.push(pos);
+        }
+    }
+
+    every_5th_row
+}
+
+fn extract_license(lines: Vec<String>, index: Vec<usize>, date: String) -> Vec<License> {
+    let mut licences: Vec<License> = Vec::new();
+
+    for i in index {
+        licences.push(License {
+            date: date.clone(),
+            well_name: lines[i][..37].trim().to_string(),
+            licence_number: lines[i][37..47].trim().to_string(),
+            mineral_rights: lines[i][47..68].trim().to_string(),
+            ground_elevation: lines[i][68..].trim().to_string(),
+            unique_identifier: lines[i+1][..37].trim().to_string(),
+            surface_coordinates: lines[i+1][37..47].trim().to_string(),
+            aer_field_centre: lines[i+1][47..68].trim().to_string(),
+            projected_depth: lines[i+1][68..].trim().to_string(),
+            aer_classification: lines[i+2][..37].trim().to_string(),
+            field: lines[i+2][37..68].trim().to_string(),
+            terminating_zone: lines[i+2][68..].trim().to_string(),
+            drilling_operation: lines[i+3][..37].trim().to_string(),
+            well_purpose: lines[i+3][37..47].trim().to_string(),
+            well_type: lines[i+3][47..68].trim().to_string(),
+            substance: lines[i+3][68..].trim().to_string(),
+            licensee: lines[i+4][..68].trim().to_string(),
+            surface_location: lines[i+4][68..].trim().to_string()
+        });
+    }
+
+    licences
+}
+
+fn write_licence_to_csv(licences: Vec<License>, filename: &str) {
+    let mut wtr = csv::Writer::from_path(format!("CSV/WELLS{}.csv", filename)).expect("Unable to create csv file");
+
+    for licence in licences {
+        wtr.serialize(licence).expect("Unable to write licence to csv file");
+    }
+
+    wtr.flush().expect("Unable to flush csv file");
+}
+
+#[derive(Debug)]
 struct Indeces {
     breaks: Vec<usize>,
     date: Vec<usize>,
     cancelled: Vec<usize>,
+    amendments: Vec<usize>,
+    updates: Vec<usize>,
 }
 
 impl Indeces {
@@ -63,6 +141,8 @@ impl Indeces {
         let mut index_breaks: Vec<usize> = Vec::new();
         let mut index_date: Vec<usize> = Vec::new();
         let mut index_cancelled: Vec<usize> = Vec::new();
+        let mut index_amendments: Vec<usize> = Vec::new();
+        let mut index_updates: Vec<usize> = Vec::new();
         
         for (pos, e) in lines_iter {
             if e.contains("---") {
@@ -74,13 +154,21 @@ impl Indeces {
             } else if e.contains("CANCELLED") {
                 index_cancelled.push(pos);
                 //println!("Element at position {}: {:?}", pos, e);
+            } else if e.contains("AMENDMENTS") {
+                index_amendments.push(pos);
+                //println!("Element at position {}: {:?}", pos, e);
+            } else if e.contains("UPDATES") {
+                index_updates.push(pos);
+                //println!("Element at position {}: {:?}", pos, e);
             }
-            };
+        }
         
         let indices = Indeces {
             breaks: index_breaks,
             date: index_date,
             cancelled: index_cancelled,
+            amendments: index_amendments,
+            updates: index_updates,
         };
 
         indices
@@ -88,110 +176,32 @@ impl Indeces {
     }
 }
 
-fn open_file_lines(filename: &str) -> Vec<String> {
-    let path = format!("TXT/{}.TXT", filename);
-    let file = File::open(path).expect("File not found");
-    let content = BufReader::new(file);
-    let lines: Vec<String> = content
-    .lines()
-    .map(|line| line.expect("Something went wrong"))
-    .collect();
 
-    lines
-}
 
-fn licences <'a>(lines: &'a Vec<String>, breaks: &'a Vec<usize>) -> Vec<&'a str> {
-    let mut licences_vec: Vec<&str> = Vec::new();
-    
-    for i in breaks[1]+1..breaks[2]-2 {
-        if lines[i].trim().len() > 0 {
-            licences_vec.push(&lines[i].trim());
-        }
-    };
+#[tokio::main]
+async fn main() {
+    let args: Vec<String> = env::args().collect();
 
-    let mut i = 0;
-    let mut licence_vec_clean_split: Vec<&str> = Vec::new();
+    let filename = &args[1];
 
-    while i < licences_vec.len() {
-        licence_vec_clean_split.push(&licences_vec[i].trim()[..37]);
-        licence_vec_clean_split.push(&licences_vec[i].trim()[37..47]);
-        licence_vec_clean_split.push(&licences_vec[i].trim()[47..68]);
-        licence_vec_clean_split.push(&licences_vec[i].trim()[68..]);
+    println!("In file {}", filename);
 
-        licence_vec_clean_split.push(&licences_vec[i+1].trim()[..37]);
-        licence_vec_clean_split.push(&licences_vec[i+1].trim()[37..47]);
-        licence_vec_clean_split.push(&licences_vec[i+1].trim()[47..68]);
-        licence_vec_clean_split.push(&licences_vec[i+1].trim()[68..]);
+    //download_file(filename.to_string()).await.unwrap();
 
-        licence_vec_clean_split.push(&licences_vec[i+2].trim()[..37]);
-        licence_vec_clean_split.push(&licences_vec[i+2].trim()[37..68]);
-        licence_vec_clean_split.push(&licences_vec[i+2].trim()[68..]);
+    let lines = open_file_lines(filename);
 
-        licence_vec_clean_split.push(&licences_vec[i+3].trim()[..37]);
-        licence_vec_clean_split.push(&licences_vec[i+3].trim()[37..47]);
-        licence_vec_clean_split.push(&licences_vec[i+3].trim()[47..68]);
-        licence_vec_clean_split.push(&licences_vec[i+3].trim()[68..]);
+    let lines_trimmed = trim_and_remove_empty_lines(lines);
 
-        licence_vec_clean_split.push(&licences_vec[i+4].trim()[..68]);
-        licence_vec_clean_split.push(&licences_vec[i+4].trim()[68..]);
+    let index = Indeces::search(&lines_trimmed);
 
-        i += 5;
-    };
+    let date = &lines_trimmed[index.date[0]].trim()[6..];
 
-    licence_vec_clean_split
+    let licences_lines = extract_licences_lines(&lines_trimmed, index.breaks);
 
-}
+    let rows = find_every_5th_row(&licences_lines);
 
-fn cancelled <'a>(lines: &'a Vec<String>, breaks: &'a Vec<usize>, cancelled: &'a Vec<usize>) -> Vec<&'a str>{
-    let mut cancelled_vec_index: Vec<usize> = Vec::new();
+    let licences = extract_license(licences_lines, rows, date.to_string());
 
-    for j in breaks {
-        if j > &cancelled[0] {
-                    cancelled_vec_index.push(*j);
-        }
-    }
+    write_licence_to_csv(licences, filename);
 
-    let mut cancelled_vec: Vec<&str> = Vec::new();
-    
-    for i in cancelled[0]+5..cancelled_vec_index[2]-2 {
-        if lines[i].trim().len() > 0 {
-            cancelled_vec.push(&lines[i].trim());
-        }
-    };
-
-    let mut c = 0;
-    let mut cancelled_vec_clean_split: Vec<&str> = Vec::new();
-
-    while c < cancelled_vec.len() {
-        cancelled_vec_clean_split.push(&cancelled_vec[c][..cancelled_vec[c].len()-8].trim());
-        cancelled_vec_clean_split.push(&cancelled_vec[c][cancelled_vec[c].len()-7..].trim());
-        c += 2;
-    }
-
-    cancelled_vec_clean_split
-
-}
-
-fn writer (licences: Vec<&str>, date: &str, x: usize) {
-    let number_licences = licences.len()/x;
-
-    let path_lic = format!("ST1_Licences_{}.csv", date);
-
-    //create a csv with the results from licence_vec_clean_split vector divided by 18 columns and write a file
-    let csv_file = File::create(path_lic).expect("Unable to create file");
-    let mut csv_writer = csv::Writer::from_writer(csv_file);
-
-    for i in 0..number_licences {
-        let mut row = Vec::new();
-        for j in 0..x {
-            row.push(licences[i*x+j].trim());
-        }
-        row.push(date);
-
-        //println!("{:?}", row);
-
-        csv_writer.write_record(&row).expect("Unable to write to file");
-    }
-
-    csv_writer.flush().expect("Unable to flush to file");
 }
